09271cadc82043cfff62d2d85650aae6502cfa9e
OVOMARK <mcncstatefan@icloud.com>
2022-12-06 22:31:48 -0500

    updated our 2nd model for database saving for post data

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
-const { before } = require('node:test');
 
 class User extends Model {
     checkPassword(loginPw) {
--- models/index.js
+++ models/index.js
@@ -1,1 +1,12 @@
 const User = require('./User');
+const Post = require('./Post');
+
+User.belongsToMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.hasOne(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = {User, Post}
