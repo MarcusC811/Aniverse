3818a2e1daecaa80485a25c9ffe9eda00b9182f1
OVOMARK <mcncstatefan@icloud.com>
2022-12-12 21:59:43 -0500

    added logic to append post data to DB

--- controllers/api/postData.js
+++ controllers/api/postData.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 const { Post, User } = require('../../models');
 
 router.post('/', async (req, res) => {
-    const postInfo = await Post.create({
-        ...req.body,
-    });
+    const postInfo = await Post.create(req.body);
 
     if(!postInfo) {
         res.status(404).json({message: 'Invalid information provided'});
