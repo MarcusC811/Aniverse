b87c2539eb7d5075ea5bb292ab119a4cb5249afb
OVOMARK <mcncstatefan@icloud.com>
2022-12-18 12:02:56 -0500

    fixed login redirect login and added support for logout button

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,34 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
 
-router.get('/', async (req, res) => {  
+router.get('/', async (req, res) => {
+    if (!req.session.user_id) {
+        res.redirect('/login')
+        } else {
     const postData = await Post.findAll();
-
     const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('homepage', { posts: posts });
+    res.render('homepage', { posts: posts, loggedIn: true });
+    }
 });
 
 router.get('/login', (req, res) => {
     if (req.session.user_id) {
         res.redirect('/profile');
         return;
-    }
+    } else {
     res.render('login');
+    }
 });
 
 
 
 router.get('/aboutus', async (req, res) => {
-    res.render('about');
+    if (!req.session.user_id) {
+        res.redirect('/login')
+        } else {
+    res.render('about', {loggedIn: true});
+    }
 });
 
 
@@ -34,13 +42,14 @@ router.get('/signup', async (req, res) => {
 router.get('/profile' , async (req, res) => {
     if (!req.session.user_id) {
     res.redirect('/login')
-    }
+    } else {
     const userData = await User.findByPk(req.session.user_id, {
         attributes: { exclude: ['password'] },
       });
 
     const user = userData.get({ plain: true });
-    res.render('profile', { ...user })
+    res.render('profile', { ...user, loggedIn: true })
+    }
 })
 
 module.exports = router;
